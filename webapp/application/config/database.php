45bcca935fc8a52a35dc159e6ee402bf3df89543
juicyboba <rizki.14117084@student.itera.ac.id>
2021-11-15 12:40:53 +0700

    update

--- webapp/application/config/database.php
+++ webapp/application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'sumb3rk4h',
-	'password' => 'X$Y%!wW=na^o',
-	'database' => 'sumb3rk4h_skb',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'skb',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
